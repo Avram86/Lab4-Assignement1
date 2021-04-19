b2ec4dd5bee2a5795a062eafd05078b34225d757
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-04-19 11:05:31 +0300

    Add project files.

---
+++ Assignement1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Assignement1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //assignment1
+            var array = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Please write a number between 1 and 100: ");
+                var input = Console.ReadLine();
+                var x = int.Parse(input);
+                array[i] = x;
+
+            }
+            AfiseazaStringCuSeparator('*', array);
+        }
+        public static void AfiseazaStringCuSeparator(char x, int[] sir)
+        {
+            for (int i = 0; i < sir.Length; i++)
+            {
+                Console.Write($"{sir[i]}{x} ");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
